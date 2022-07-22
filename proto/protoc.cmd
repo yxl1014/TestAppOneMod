a0852ffc86fd638323ccd05f86842089e4328165
余鑫磊 <1627419257@qq.com>
2022-07-22 16:43:07 +0800

    TestApp V2.0 客户端对服务器的请求接口，以及使用protobuf

---
+++ proto/protoc.cmd
@@ -0,0 +1,7 @@
+%~d0
+cd %~dp0
+protoc3_21_3_win64.exe --version
+protoc3_21_3_win64.exe --java_out=..\src\main\java ShenXian.proto
+protoc3_21_3_win64.exe --java_out=..\src\main\java data.proto
+:protoc3_21_3_win64.exe --java_out=C:\Users\Administrator\Desktop\angelGJ\Workspaces\cross-server\src ShenXian.proto
+pause
---
+++ src/main/java/yxl/UserAndTask/controller/ServerController.java
@@ -0,0 +1,15 @@
+package yxl.UserAndTask.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: yxl
+ * @Date: 2022/7/22 11:39
+ */
+
+@RestController
+@RequestMapping("/server")
+public class ServerController {
+
+}
